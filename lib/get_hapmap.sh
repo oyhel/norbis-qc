2dd78cb55bf02914fbf2e1e6b3cdfeb30e49cc9d
Øyvind Helgeland <helgeland.oyvind@gmail.com>
2018-06-05 09:16:10 +0200

    removed old pdf

--- lib/get_hapmap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget http://hapmap.ncbi.nlm.nih.gov/downloads/genotypes/hapmap3_r3/relationships_w_pops_041510.txt
-wget http://hapmap.ncbi.nlm.nih.gov/downloads/genotypes/hapmap3_r3/plink_format/00README.txt
-wget http://hapmap.ncbi.nlm.nih.gov/downloads/genotypes/hapmap3_r3/plink_format/hapmap3_r3_b36_fwd.consensus.qc.poly.map.gz
-wget http://hapmap.ncbi.nlm.nih.gov/downloads/genotypes/hapmap3_r3/plink_format/hapmap3_r3_b36_fwd.consensus.qc.poly.ped.gz
